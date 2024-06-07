066053d0b07084862ab674929e3ceab95b91c54f
jasonlin1993 <ninjachen.lin@gmail.com>
2024-06-07 14:13:12 +0800

    第 15 題

--- wehelp_15.js
+++ wehelp_15.js
@@ -27,8 +27,9 @@ function checkPassword(s) {
     /[A-Z]/.test(s) &&
     /[a-z]/.test(s) &&
     /[0-9]/.test(s) &&
-    /[!@#$%]/.test(s)
+    /[!@#$%]/.test(s) &&
+    /^[A-Za-z0-9!@#$%]+$/.test(s)
   );
 }
 
-console.log(checkPassword("aBcdefg8*"));
+console.log(checkPassword("AVbG789$%6"));
