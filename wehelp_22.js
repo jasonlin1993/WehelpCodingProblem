081586a0d2bdd62203eb5d6a4faf3209f26603ee
jasonlin1993 <ninjachen.lin@gmail.com>
2024-06-08 11:40:44 +0800

    第 22 題

---
+++ wehelp_22.js
@@ -0,0 +1,29 @@
+/*
+22. 平方根，四捨五入取整數
+輸入一個正整數，你的函式可以計算這個正整數的平方根，但請四捨五入到整數的部份。
+
+輸入範例二：9
+回傳：3
+
+輸入範例一：12
+回傳：3
+
+輸入範例一：15
+回傳：4
+
+*/
+function sqrt(n) {
+  let sqrtAns;
+  sqrtAns = Math.sqrt(n);
+  return Math.round(sqrtAns);
+}
+
+console.log(sqrt(15));
+
+/*
+function sqrt(n) {
+    return Math.round(Math.sqrt(n))
+
+}
+
+*/
